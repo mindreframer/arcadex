f6b2401fd674fa35003628dae2d507d7902fe182
Roman Heinrich <roman.heinrich@gmail.com>
2025-11-19 06:20:24 +0100

    Chore: dummy runner

---
+++ lib/runner.ex
@@ -0,0 +1,19 @@
+defmodule Runner do
+  def sqlscript(db, script) do
+    url = "http://localhost:2480/api/v1/command/#{db}"
+    payload = %{language: "sqlscript", command: script}
+
+    send_command(url, payload)
+  end
+
+  def sql(db, query) do
+    url = "http://localhost:2480/api/v1/query/#{db}"
+    payload = %{language: "sql", command: query}
+
+    send_command(url, payload)
+  end
+
+  def send_command(url, payload) do
+    Req.post!(url, json: payload, auth: {:basic, "root:playwithdata"})
+  end
+end
